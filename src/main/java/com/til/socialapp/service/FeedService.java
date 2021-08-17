d9cc989d8179716db281c7281023c6df6c35efb0
Divey Jain <86945791+diveyjain28@users.noreply.github.com>
2021-08-17 17:11:07 +0530

    Fixed Feed API Error reported by Jai.

--- src/main/java/com/til/socialapp/service/FeedService.java
+++ src/main/java/com/til/socialapp/service/FeedService.java
@@ -5,14 +5,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import com.til.socialapp.repository.EmployeeRepository;
 import com.til.socialapp.repository.PostRepository;
+
+import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
+
 import com.til.socialapp.model.Post;
 
 @Service
 public class FeedService {
 	@Autowired
 	private PostRepository post;
-
+	@Autowired
+    private EmployeeRepository emp;
 	public Page<Post> getFeed(String sorted, int empId, String type, int page) {
 		Pageable pageable = PageRequest.of(page, 5);
 		Page<Post> feed = null;
@@ -28,6 +34,13 @@ public class FeedService {
 			} else {
 				feed = post.findByEmpIdOrderByLikesCountDesc(empId, pageable);
 			}
+		}
+		for(int i=0;i<feed.getNumberOfElements();i++)
+		{
+			Post p=feed.getContent().get(i);
+			p.setDesignation(emp.findByempId(p.getEmpId()).getDesignation());
+			p.setName(emp.findByempId(p.getEmpId()).getName());
+			p.setEmpImgUrl(emp.findByempId(p.getEmpId()).getImgUrl());
 		}
 		return feed;
 	}
