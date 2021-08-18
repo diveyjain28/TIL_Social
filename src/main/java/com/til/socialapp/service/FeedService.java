d31c852d92b42fa5d21bea8599ceefe6b055d851
souravnitkkr1 <kumar.sourav@timesinternet.in>
2021-08-18 11:56:33 +0530

    Worked on interest part of Feed API

--- src/main/java/com/til/socialapp/service/FeedService.java
+++ src/main/java/com/til/socialapp/service/FeedService.java
@@ -1,32 +1,42 @@
 package com.til.socialapp.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import com.til.socialapp.repository.EmployeeRepository;
 import com.til.socialapp.repository.PostRepository;
+import com.til.socialapp.model.Employee;
 import com.til.socialapp.model.Post;
 
 @Service
 public class FeedService {
 	@Autowired
 	private PostRepository post;
-
+	@Autowired
+	private EmployeeRepository emp;
 	public Page<Post> getFeed(String sorted, int empId, String type, int page) {
 		Pageable pageable = PageRequest.of(page, 5);
 		Page<Post> feed = null;
+		Employee e=emp.findByempId(empId);
 		if (type.equals("feed")) {
 			if (sorted.equals("recency")) {
-				feed = post.findByEmpIdNotOrderByCreatedAtDesc(empId, pageable);
+				feed = post.findByEmpIdNotAndTagsInOrderByCreatedAtDesc(empId,e.getInterests(), pageable);
 			} else {
-				feed = post.findByEmpIdNotOrderByLikesCountDesc(empId, pageable);
+				feed = post.findByEmpIdNotAndTagsInOrderByLikesCountDesc(empId,e.getInterests(), pageable);
 			}
 		} else if (type.equals("self")) {
 			if (sorted.equals("recency")) {
-				feed = post.findByEmpIdOrderByCreatedAtDesc(empId, pageable);
+				feed = post.findByEmpIdAndTagsInOrderByCreatedAtDesc( empId,e.getInterests(), pageable);
 			} else {
-				feed = post.findByEmpIdOrderByLikesCountDesc(empId, pageable);
+				feed = post.findByEmpIdAndTagsInOrderByLikesCountDesc(empId,e.getInterests(), pageable);
 			}
 		}
 		return feed;
